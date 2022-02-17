6517697c9f799fe94458246f0d92fb55e4c2e69e
Matt <mgstankov30@gmail.com>
2022-02-16 20:04:50 -0500

    adding the completed spinner2 code crediting @MikeB84 for help with the percentage issue and @yani5752000 for assiting with refactoring to make it work better

--- spinner2.js
+++ spinner2.js
@@ -1,7 +1,16 @@
-const sentance = "|/-\|/-\|\n";
+// adding the sentance structure to account for the different animation styles.
+const sentance = "|/-\|/-\|";
+// pushing the setnance to an array with .split.
 let arr = sentance.split('');
+// looping through the index of the array with a for in.
 for (const index in arr) {
+  //setting the timeout for the array to use the \r function + array index + a space.
     setTimeout(() => {
-    process.stdout.write('\r'+arr[index]);
+    process.stdout.write('\r'+arr[index]+" ");
+    // times the index by 300 ms to rotate evenly.
   }, 300 * index);
 };
+// setting an extra time out to remove the percentage @MikeB84 is credited for this assit!
+setTimeout(() => {
+  console.log();
+}, 1800);
